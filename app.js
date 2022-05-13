3c60f3fd6098b4d5be9d0e777595528b9033b2cd
DmitryBavelko-98 <bavelko90@gmail.com>
2022-05-13 16:23:00 +0400

    Fix board and range

--- app.js
+++ app.js
@@ -1,56 +1,52 @@
 'use strict';
 
-const board = document.querySelector('.board');
-const select = document.querySelector('.select');
-const range = document.querySelector('#range');
-const label = document.querySelector('#label');
-const red = document.querySelector('#red');
-const black = document.querySelector('#black');
-const randomColor = document.querySelector('#random');
-const eraser = document.querySelector('#eraser');
-const clear = document.querySelector("#clear");
+const board = document.querySelector('.board'),
+    select = document.querySelector('.select'),
+    range = document.querySelector('#range'),
+    label = document.querySelector('#label'),
+    red = document.querySelector('#red'),
+    black = document.querySelector('#black'),
+    randomColor = document.querySelector('#random'),
+    eraser = document.querySelector('#eraser'),
+    clear = document.querySelector("#clear"),
+    tabletScreen = window.matchMedia('(max-width: 528px)'),
+    mobileScreen = window.matchMedia('(max-width: 425px)');
 
-let width = 0;
-let height  = 0;
-let rgba = 0;
-let count = range.value;
+let width = 0,
+    height  = 0,
+    rgba = 0,
+    count = 16,
+    boardWidth,
+    boardHeight,
+    blocks;
 
-let boardWidth;
-let boardHeight;
-
-const tabletScreen = window.matchMedia('(max-width: 528px)');
+    
 function handleTabletChange(e) {
   if (e.matches) {
+    let count = range.value;
     boardWidth = parseFloat(window.getComputedStyle(board).width);
     boardHeight = parseFloat(window.getComputedStyle(board).height);
     setBlocksSize(count);
   } else {
+    let count = range.value;
     boardWidth = 500;
     boardHeight = 500; 
     setBlocksSize(count);
   }
 }
 
-tabletScreen.addListener(handleTabletChange);
-handleTabletChange(tabletScreen);
-
-const mobileScreen = window.matchMedia('(max-width: 425px)')
 function handleMobileChange(e) {
   if (e.matches) {
+    let count = range.value;
     boardWidth = parseFloat(window.getComputedStyle(board).width);
     boardHeight = parseFloat(window.getComputedStyle(board).height);
     setBlocksSize(count);
   } else {
+    let count = range.value;
     handleTabletChange(tabletScreen);
     setBlocksSize(count);
   }
 }
-
-mobileScreen.addListener(handleMobileChange)
-handleMobileChange(mobileScreen);
-
-
-let blocks;
 
 function setCount (count = 16) {
     for (let i = 0; i < count**2; i++) {
@@ -60,9 +56,13 @@ function setCount (count = 16) {
     }
     blocks = document.querySelectorAll('.block');
     setBlocksSize(count);
+    draw();
+}
+
+function draw() {
     blocks.forEach(block => {  
         block.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor =  'red';
+            e.target.style.backgroundColor =  'rgba(0, 0, 0, 0.1)';
         });    
         red.addEventListener('click', () => {
             block.addEventListener('mouseover', (e) => {
@@ -85,22 +85,24 @@ function setCount (count = 16) {
                 e.target.style.backgroundColor = '#fff';
             });
         });
-        clear.addEventListener('click', () => {
+        clear.addEventListener('click', (e) => {
             blocks.forEach(block => {
                 block.style.backgroundColor = '#fff';
             });
         });
+        clear.onfocus = () => {
+            clear.blur();
+        }
+        clear.onblur = (e) => {
+            draw();
+        }
     });
 }
-
-setCount();
 
 function setBlocksSize (count) {
     let blocks = document.querySelectorAll('.block');
     width = boardWidth / count;
     height = boardHeight / count;
-    console.log(boardWidth);
-    console.log(width);
     blocks.forEach(block => {
         block.style.width = `${width}px`;
         block.style.height = `${height}px`;
@@ -114,6 +116,8 @@ function clearBoard () {
     }) 
 }
 
+setCount();
+
 range.addEventListener('change', () => {
     label.replaceChildren();
     let value = range.value;
@@ -122,6 +126,12 @@ range.addEventListener('change', () => {
     setBlocksSize(value);
     label.insertAdjacentHTML('afterbegin', `${value} x ${value}`);
 });
+
+tabletScreen.addListener(handleTabletChange);
+handleTabletChange(tabletScreen);
+
+mobileScreen.addListener(handleMobileChange)
+handleMobileChange(mobileScreen);
 
 
 
